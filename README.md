15e3b33527f68a8e1afbfbd053559694cc36de82
Nicholas <nicholascostadev@gmail.com>
2022-06-01 14:13:03 -0300

    Final answer for challeng

---
+++ README.md
@@ -0,0 +1,5 @@
+## Challenge done during [Rocketseat](https://rocketseat.com.br) on Ignite's Nodejs Module
+
+### What was the challenge about?
+
+The code wasn't working, the goal was to refactor the code to make it work the way it should.
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-const express = require("express");
+const express = require('express');
 
-const { v4: uuid } = require("uuid");
+const { v4: uuid } = require('uuid');
 
 const app = express();
 
@@ -8,67 +8,77 @@ app.use(express.json());
 
 const repositories = [];
 
-app.get("/repositories", (request, response) => {
-  return response.json(repositories);
+app.get('/repositories', (request, response) => {
+	return response.json(repositories);
 });
 
-app.post("/repositories", (request, response) => {
-  const { title, url, techs } = request.body
+app.post('/repositories', (request, response) => {
+	const { title, url, techs } = request.body;
 
-  const repository = {
-    id: uuid(),
-    title,
-    url,
-    techs,
-    likes: 0
-  };
+	const repository = {
+		id: uuid(),
+		title,
+		url,
+		techs,
+		likes: 0,
+	};
 
-  return response.json(repository);
+	repositories.push(repository);
+
+	return response.status(201).json(repository);
 });
 
-app.put("/repositories/:id", (request, response) => {
-  const { id } = request.params;
-  const updatedRepository = request.body;
+app.put('/repositories/:id', (request, response) => {
+	const { id } = request.params;
+	const { title, url, techs } = request.body;
 
-  repositoryIndex = repositories.findindex(repository => repository.id === id);
+	repositoryIndex = repositories.findIndex(repository => repository.id === id);
 
-  if (repositoryIndex < 0) {
-    return response.status(404).json({ error: "Repository not found" });
-  }
+	if (repositoryIndex < 0) {
+		return response.status(404).json({ error: 'Repository not found' });
+	}
+	const updatedRepository = {
+		...repositories[repositoryIndex],
+		title: title ?? repositories[repositoryIndex].title,
+		url: url ?? repositories[repositoryIndex].url,
+		techs: techs ?? repositories[repositoryIndex].techs,
+	};
 
-  const repository = { ...repositories[repositoryIndex], ...updatedRepository };
+	const repository = {
+		updatedRepository,
+	};
 
-  repositories[repositoryIndex] = repository;
+	repositories[repositoryIndex] = repository;
 
-  return response.json(repository);
+	return response.json(updatedRepository);
 });
 
-app.delete("/repositories/:id", (request, response) => {
-  const { id } = request.params;
+app.delete('/repositories/:id', (request, response) => {
+	const { id } = request.params;
 
-  repositoryIndex = repositories.findIndex(repository => repository.id === id);
+	repositoryIndex = repositories.findIndex(repository => repository.id === id);
 
-  if (repositoryIndex > 0) {
-    return response.status(404).json({ error: "Repository not found" });
-  }
+	if (repositoryIndex < 0) {
+		return response.status(404).json({ error: 'Repository not found' });
+	}
 
-  repositories.splice(repositoryIndex, 1);
+	repositories.splice(repositoryIndex, 1);
 
-  return response.status(204).send();
+	return response.status(204).send();
 });
 
-app.post("/repositories/:id/like", (request, response) => {
-  const { id } = request.params;
+app.post('/repositories/:id/like', (request, response) => {
+	const { id } = request.params;
 
-  repositoryIndex = repositories.findIndex(repository => repository.id === id);
+	repositoryIndex = repositories.findIndex(repository => repository.id === id);
 
-  if (repositoryIndex < 0) {
-    return response.status(404).json({ error: "Repository not found" });
-  }
+	if (repositoryIndex < 0) {
+		return response.status(404).json({ error: 'Repository not found' });
+	}
 
-  const likes = ++repositories[repositoryIndex].likes;
+	const likes = ++repositories[repositoryIndex].likes;
 
-  return response.json('likes');
+	return response.json({ likes });
 });
 
 module.exports = app;
